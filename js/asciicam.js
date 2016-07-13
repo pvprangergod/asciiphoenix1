e2ba396850017750e30dffd36b9d8a903c556f8a
pvprangergod <bmasked7@gmail.com>
2016-07-13 17:43:25 +1200

    Update asciicam.js

--- js/asciicam.js
+++ js/asciicam.js
@@ -2,8 +2,8 @@
 	var cam, intervalId, canvas, canvasCtx, ascii, btnStart, btnStop;
 
 	var loopSpeed = 100;
-	var width = 230;
-	var height = 150;
+	var width = 250;
+	var height = 141;
 
     app.init = function () {
 		//Get all the page element we need
