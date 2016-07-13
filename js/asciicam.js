26d0014f12e7fd1c439de46ea9bbc96356a9c219
pvprangergod <bmasked7@gmail.com>
2016-07-13 19:28:54 +1200

    Update asciicam.js

--- js/asciicam.js
+++ js/asciicam.js
@@ -59,7 +59,7 @@
     //http://thecodeplayer.com/walkthrough/cool-ascii-animation-using-an-image-sprite-canvas-and-javascript
     app.loop = function () {
 		var r, g, b, gray;
-		var character, line = "\033[32m";
+		var character, line = "";
 		
 		//clear canvas
 		canvasCtx.clearRect (0, 0, width, height);
